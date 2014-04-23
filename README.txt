647eed9698500660d4d946df9a606318606fa45d
Anthony Kohan <anthony@bismuth.(none)>
2014-04-23 15:54:15 -0700

    changes to database tables and adding ability to extract from multiple package types

--- README.txt
+++ README.txt
@@ -1,4 +1,8 @@
+Discription:
+-Takes a package file and extracts it to a temporary directory.
+-It then runs ninka on every extracted file and stores the content of the files outputed by ninka in a sqlite database.
+
 Operation:
 
 -Move ninka-wrapper.pl to root directory of ninka
--Execute using syntax "perl ninka-wrapper.pl <path to tar file> <databasename>"
+-Execute using syntax "perl ninka-wrapper.pl <path to package file> <databasename>"
--- ninka-wrapper.pl
+++ ninka-wrapper.pl
@@ -9,17 +9,45 @@ use File::Basename;
 
 if(scalar(@ARGV) != 2){
     print STDERR "Incorrect number of arguments\n";
-    print STDERR "Correct usage is: perl ninka-wrapper <path to tar file> <database name>\n";
+    print STDERR "Correct usage is: perl ninka-wrapper <path to package file> <database name>\n";
     exit 1;
 }
 
-my ($file, $db) = @ARGV;
+my ($pack, $db) = @ARGV;
+
+my $dbh = DBI->connect("DBI:SQLite:dbname=$db", "", "", {RaiseError => 1})
+    or die $DBI::errstr;
+$dbh->do("CREATE TABLE IF NOT EXISTS
+          comments (filename TEXT, path TEXT, container TEXT, content TEXT,
+          PRIMARY KEY(filename, path, container))");
+$dbh->do("CREATE TABLE IF NOT EXISTS 
+          sentences (filename TEXT, path TEXT, container TEXT, content TEXT,
+          PRIMARY KEY(filename, path, container))");
+$dbh->do("CREATE TABLE IF NOT EXISTS 
+          goodsents (filename TEXT, path TEXT, container TEXT, content TEXT,
+          PRIMARY KEY(filename, path, container))");
+$dbh->do("CREATE TABLE IF NOT EXISTS
+          badsents (filename TEXT, path TEXT, container TEXT, content TEXT,
+          PRIMARY KEY(filename, path, container))");
+$dbh->do("CREATE TABLE IF NOT EXISTS
+          senttoks (filename TEXT, path TEXT, container TEXT, content TEXT,
+          PRIMARY KEY(filename, path, container))");
+$dbh->do("CREATE TABLE IF NOT EXISTS
+          licenses (filename TEXT, path TEXT, container TEXT, content TEXT,
+          PRIMARY KEY(filename, path, container))");
 
 my $tempdir = File::Temp->newdir();
 my $dirname = $tempdir->dirname;
 
-print "***** Extracting file [$file] to temporary directory [$dirname] *****\n";
-my $output = execute("tar -xvf '$file' --directory '$dirname'");
+print "***** Extracting file [$pack] to temporary directory [$dirname] *****\n";
+my $packext = getExtension($pack);
+if ($packext eq ".bz2" || $packext eq ".gz") {
+    execute("tar -xvf '$pack' --directory '$dirname'");
+} elsif ($packext eq ".jar" || $packext eq ".zip") {
+    execute("unzip -d $dirname $pack");
+} else {
+    print "ninka-wrapper does not support packages with extension [$packext]\n";
+}
 
 my @files;
 find(
@@ -30,24 +58,8 @@ find(
 print "***** Beginning Execution of Ninka *****\n";
 foreach my $file (@files) {
     print "Running ninka on file [$file]\n";
-    $output = execute("perl ./ninka.pl '$file'");
+    execute("perl ../ninka/ninka.pl '$file'");
 }
-
-my $dbh = DBI->connect("DBI:SQLite:dbname=$db", "", "", {RaiseError => 1})
-    or die $DBI::errstr;
-
-$dbh->do("CREATE TABLE IF NOT EXISTS
-          comments (filename TEXT, content TEXT)");
-$dbh->do("CREATE TABLE IF NOT EXISTS 
-          sentences (filename TEXT, content TEXT)");
-$dbh->do("CREATE TABLE IF NOT EXISTS 
-          goodsents (filename TEXT, content TEXT)");
-$dbh->do("CREATE TABLE IF NOT EXISTS
-          badsents (filename TEXT, content TEXT)");
-$dbh->do("CREATE TABLE IF NOT EXISTS
-          senttoks (filename TEXT, content TEXT)");
-$dbh->do("CREATE TABLE IF NOT EXISTS
-          licenses (filename TEXT, content TEXT)");
 
 my @ninkafiles;
 find(
@@ -63,8 +75,11 @@ find(
 print "***** Entering Ninka Data into Database [$db] *****\n";
 foreach my $file (@ninkafiles) {
     
+    my $filepath = dirname($file);
+    $filepath =~ s/$dirname//;
     my $basefile = basename($file);
     my $rootfile = removeExtension($basefile);
+    my $packname = basename($pack);
 
     #Read entire file into a string
     open (my $fh, '<', $file) or die "Can't open file $!";
@@ -74,31 +89,33 @@ foreach my $file (@ninkafiles) {
     switch (getExtension($basefile)){
 	case ".comments" {
 	    print "Inserting [$basefile] into table comments\n";
-	    $sth = $dbh->prepare("INSERT INTO comments VALUES(?, ?)");
+	    $sth = $dbh->prepare("INSERT INTO comments VALUES(?, ?, ?, ?)");
 	}
 	case ".sentences" {
 	    print "Inserting [$basefile] into table sentences\n";
-	    $sth = $dbh->prepare("INSERT INTO sentences VALUES(?, ?)");
+	    $sth = $dbh->prepare("INSERT INTO sentences VALUES(?, ?, ?, ?)");
 	}
 	case ".goodsent" {
 	    print "Inserting [$basefile] into table goodsents\n";
-	    $sth = $dbh->prepare("INSERT INTO goodsents VALUES(?, ?)");
+	    $sth = $dbh->prepare("INSERT INTO goodsents VALUES(?, ?, ?, ?)");
 	}
 	case ".badsent" {
 	    print "Inserting [$basefile] into table goodsents\n";
-	    $sth = $dbh->prepare("INSERT INTO badsents VALUES(?, ?)");
+	    $sth = $dbh->prepare("INSERT INTO badsents VALUES(?, ?, ?, ?)");
 	}
 	case ".senttok" {
 	    print "Inserting [$basefile] into table senttoks\n";
-	    $sth = $dbh->prepare("INSERT INTO senttoks VALUES(?, ?)");
+	    $sth = $dbh->prepare("INSERT INTO senttoks VALUES(?, ?, ?, ?)");
 	}
 	case ".license" {
 	    print "Inserting [$basefile] into table licenses\n";
-	    $sth = $dbh->prepare("INSERT INTO licenses VALUES(?, ?)");
+	    $sth = $dbh->prepare("INSERT INTO licenses VALUES(?, ?, ?, ?)");
 	}
     }
     $sth->bind_param(1, $rootfile);
-    $sth->bind_param(2, $filedata);
+    $sth->bind_param(2, $filepath);
+    $sth->bind_param(3, $packname);
+    $sth->bind_param(4, $filedata);
     $sth->execute;
     close($fh);
 }
